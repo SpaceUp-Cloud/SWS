33fb4f8f787f0377dfe7200275d448cea97f5712
Thraax Session <thraax.session@iatlas.technology>
2021-09-11 20:32:16 +0200

    set up tests

--- src/test/kotlin/technology/iatlas/sws/ServerWebScriptTest.kt
+++ src/test/kotlin/technology/iatlas/sws/ServerWebScriptTest.kt
@@ -11,23 +11,14 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-plugins {
-    kotlin("jvm") version "1.5.30"
-}
+package technology.iatlas.sws
 
-group = "technology.iatlas.sws"
-version = "1.0-SNAPSHOT"
+import org.junit.jupiter.api.Test
 
-repositories {
-    mavenCentral()
-}
+internal class ServerWebScriptTest {
 
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.21")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
-    testImplementation(kotlin("test"))
-}
+    @Test
+    fun parse() {
 
-tasks.test {
-//    useJUnitPlatform()
+    }
 }
