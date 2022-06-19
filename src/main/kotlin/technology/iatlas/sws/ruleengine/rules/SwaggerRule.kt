35fb6e6487b174c2cc95a264f0dc03b9f97533b3
Thraax Session <thraax.session@iatlas.technology>
2022-06-19 22:46:14 +0200

    Swagger doc should be optional

--- src/main/kotlin/technology/iatlas/sws/ruleengine/rules/SwaggerRule.kt
+++ src/main/kotlin/technology/iatlas/sws/ruleengine/rules/SwaggerRule.kt
@@ -1,20 +1,34 @@
+/*
+ * Copyright(c) 2022 thraax.session@gino-atlas.de.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package technology.iatlas.sws.ruleengine.rules
 
+import org.apache.logging.log4j.kotlin.Logging
 import technology.iatlas.sws.ServerWebScript
-import technology.iatlas.sws.objects.ParserException
 import java.io.File
 
-class SwaggerRule : BaseRule("SWAGGER_RULE") {
+class SwaggerRule : BaseRule("SWAGGER_DOC"), Logging {
 
     override fun process(sws: ServerWebScript, swsFile: File): ServerWebScript {
         super.process(sws, swsFile)
-        val regexRule = Regex("(SWAGGER_DOC:).+(\"{3})((.|\\s)*?)(?=\"{3})")
+        val regexRule = Regex("(${this.rule}:).+(\"{3})((.|\\s)*?)(?=\"{3})")
 
         val result = regexRule.find(swsFile.readText())?.groupValues
         if (result != null) {
             sws.swaggerDoc = result[3].trim()
         } else {
-            throw ParserException("Could not parse SWAGGER_DOC!")
+            //throw ParserException("Could not parse ${this.rule}!")
+            logger.warn("Ignore ${this.rule} as it is not defined.")
         }
         return sws
     }
